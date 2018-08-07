ee69dce5b3c3fdbc4269c8e82b6fb79b5eddc378
Michael Coons <michael@Michaels-MacBook-Pro-3.local>
2018-08-07 14:51:29 -0600

    changed seeds

--- seeds/games.js
+++ seeds/games.js
@@ -3,9 +3,9 @@ exports.seed = function(knex, Promise) {
   return knex('game').del()
     .then(function () {
       return knex('game').insert([
-        {name: 'Breath of the Wild', developer: 'Nintendo', rating: 8.5},
-        {name: 'Fez', developer: 'Polytron', rating: 7.4},
-        {name: 'Anti-Chamber', developer: 'Demruth', rating: 8.1}
+        {id: 1, name: 'Breath of the Wild', developer: 'Nintendo', rating: 8.5},
+        {id: 2, name: 'Fez', developer: 'Polytron', rating: 7.4},
+        {id: 3, name: 'Anti-Chamber', developer: 'Demruth', rating: 8.1}
       ]);
     })
     .then(() => {
